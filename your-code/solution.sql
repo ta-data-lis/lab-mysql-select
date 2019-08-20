36b125cb128198cf26d00b91d8e477f89ebee1e3
naivm <miguel.vian@live.com.pt>
2019-08-20 16:08:38 +0100

    updated

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -1,41 +1,42 @@
 
 # 1
-select titleauthor.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, titles.title as Title, publishers.pub_name as Publisher
-from titles
-join publishers on publishers.pub_id = titles.pub_id
+select authors.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, titles.title as Title, publishers.pub_name as Publisher
+from titles  # takes more than one argument
+inner join publishers on publishers.pub_id = titles.pub_id
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on titleauthor.au_id = authors.au_id
-#group by titleauthor.pub_id
 ;
 
 # 2
 select titleauthor.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, count(titles.title) as `#Titles`, publishers.pub_name as Publisher
-from titles
+from titles # takes more than one argument
 join publishers on publishers.pub_id = titles.pub_id
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on titleauthor.au_id = authors.au_id
-group by titleauthor.au_id
+group by titleauthor.au_id , publishers.pub_name
 ;
 
 # 3
-select titleauthor.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, count(titles.title) as `Total Titles`
+select * from sales;
+
+select titleauthor.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, count(sales.title_id) as `Total Titles Sold`
 from titles
-join publishers on publishers.pub_id = titles.pub_id
 join titleauthor on titles.title_id = titleauthor.title_id
 join authors on titleauthor.au_id = authors.au_id
-group by titleauthor.au_id
-order by `Total Titles` desc
+join sales on sales.title_id = titles.title_id
+group by authors.au_id
+order by `Total Titles Sold` desc
 limit 0,3
 ;
 
 #4
-select authors.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, count(titles.title) as `Total Titles`
+select authors.au_id as Author_id, authors.au_lname as Author_LastName, authors.au_fname as Author_FirstName, count(sales.title_id) as `Total Titles Sold`
 from titles
-join publishers on publishers.pub_id = titles.pub_id
+join sales on sales.title_id = titles.title_id
 join titleauthor on titles.title_id = titleauthor.title_id
 right join authors on titleauthor.au_id = authors.au_id
 group by authors.au_id
-order by `Total Titles` desc
+order by `Total Titles Sold` desc
 limit 0,23
 ;
 
