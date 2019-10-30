09e90bf532767cc091c2031d71a9f7e0edb1105c
Luke Smalley <luke.smalley@gmail.com>
2019-10-30 17:39:50 +0000

    completed

---
+++ your-code/solution.sql
@@ -0,0 +1,42 @@
+#Challenge 1
+SELECT authors.au_id AS 'Author ID', au_fname AS 'First Name', au_lname AS 'Last Name', title AS 'Publication Title', pub_name AS 'Publisher'
+FROM authors
+LEFT JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles
+ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers
+ON publishers.pub_id = titles.pub_id
+ORDER BY au_lname;
+
+#Challenge 2
+SELECT authors.au_id AS 'Author ID', au_fname AS 'First Name', au_lname AS 'Last Name', title AS 'Publication Title', pub_name AS 'Publisher', COUNT(titleauthor.au_id)
+FROM authors
+LEFT JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles
+ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers
+ON publishers.pub_id = titles.pub_id
+GROUP BY title;
+
+#Challenge 3
+SELECT authors.au_id, au_fname, au_lname, SUM(titles.ytd_sales)
+FROM authors
+INNER JOIN titleauthor
+ON  authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(titles.ytd_sales) DESC
+LIMIT 3;
+
+#Challenge 4
+SELECT authors.au_id, au_fname, au_lname, SUM(titles.ytd_sales)
+FROM authors
+INNER JOIN titleauthor
+ON  authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(titles.ytd_sales) DESC;
