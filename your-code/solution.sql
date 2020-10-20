21ea82c513423de59829e0b342ffdb4febb9ff2e
AfonsoPC1 <afonsocaldeira1@gmail.com>
2020-10-20 10:32:28 +0100

    Done2

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -29,7 +29,7 @@ ON publication.authors.au_id=newtable2.Author_ID
 GROUP BY Author_Id,newtable2.Pub_Name
 
 /*Challenge 3*/
-SELECT  publication.authors.au_id as Author_ID, publication.authors.au_lname as FName, publication.authors.au_fname as LName, newtable2.Total 
+SELECT  publication.authors.au_id as Author_ID, publication.authors.au_lname as FName, publication.authors.au_fname as LName, sum(newtable2.Total) as Total
 FROM	publication.authors
 INNER JOIN 
 (SELECT publication.titleauthor.au_id AS Author_ID, newtable.Title_ID, newtable.Title, newtable.Total
@@ -44,18 +44,21 @@ ORDER BY Total DESC
 LIMIT 3;
 
 /*Challenge 4*/
-SELECT  publication.authors.au_id as Author_ID, publication.authors.au_lname as FName, publication.authors.au_fname as LName, coalesce(newtable2.Total,0)
-FROM	publication.authors
+SELECT newtable3.Author_ID, newtable3.FName, newtable3.LName, sum(newtable3.Total) AS Total
+FROM
+(SELECT  publication.authors.au_id as Author_ID, publication.authors.au_lname as FName, publication.authors.au_fname as LName, coalesce(newtable2.Total,0) AS Total
+FROM publication.authors
 LEFT JOIN
-(SELECT publication.titleauthor.au_id AS Author_ID, newtable.Title_ID, newtable.Title, newtable.Total
+(SELECT publication.titleauthor.au_id AS Author_ID, newtable.Title_ID AS Title_ID, newtable.Total AS Total
 FROM publication.titleauthor
 LEFT JOIN 
 (SELECT publication.titles.title_id as Title_ID, publication.titles.title as Title, publication.titles.ytd_sales as Total
 FROM publication.titles) AS newtable
-ON publication.titleauthor.title_id = newtable.Title_ID
-GROUP BY Author_ID) as newtable2
-ON publication.authors.au_id=newtable2.Author_ID
-ORDER BY coalesce(newtable2.Total,0) DESC;
+ON publication.titleauthor.title_id = newtable.Title_ID) as newtable2
+ON publication.authors.au_id=newtable2.Author_ID) AS newtable3
+GROUP BY Author_ID
+ORDER BY Total DESC;
+
 
 	
 
